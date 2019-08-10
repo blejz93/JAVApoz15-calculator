69cf325107f60aa872f33b635fcbdb56d8b61028
Kochański Cezary <ceko@jcommerce.pl>
2019-08-10 18:27:13 +0200

    Add tests

--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -27,4 +27,30 @@ public class CalculatorTest {
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
     }
+
+    @Test
+    public void shouldReturnExpectedResultForGivenArgumentsOnSubtractionOperation() {
+        //given
+        double arg1 = 2.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(-1.0, actual, 0.00000001);
+    }
+
+    @Test
+    public void shouldReturnExpectedResultForGivenArgumentsOnMultiplicationOperation() {
+        //given
+        double arg1 = 2.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(6.0, actual, 0.00000001);
+    }
 }
